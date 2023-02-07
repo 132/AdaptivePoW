4810150b892179f44976e6050aa9b64711b0b85f
Pseth3 <prateeksethi814@gmail.com>
2023-02-07 18:46:09 -0500

    Completed the final code

--- FL/BlockFL.py
+++ FL/BlockFL.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 import random
+import sys
+import getopt
 import cv2
 from imutils import paths
 from sklearn.model_selection import train_test_split
@@ -10,6 +12,7 @@ from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D 
 from tensorflow.keras.layers import MaxPooling2D
@@ -87,26 +90,25 @@ def  sum_scaled_weights(scaled_weight_list):
     return avg_grad
 
 
-'''
+def client_main(folder_path='/home/nextg3/Documents/Thesis/Code/FL/local_models',img_path='/home/nextg3/Documents/Thesis/Code/FL/MNIST_training',client_name='client'):
+    '''
     Main fuction called for a client for
     local model training and sharing models
-'''
-def client_main(dir_path, scaling_factor=10):
+    '''
+    print('######################')
+    print('######################')
+    print('I am a client called {}'.format(client_name))
     #  declare path
     # img_path = '/home/nextg3/Documents/FederatedLearning/FL_final/MNIST_training'
-    img_path = '/home/nextg3/Documents/Thesis/Code/FL/MNIST_training'
     # Get path list
     image_paths = list(paths.list_images(img_path))
+    print('Reading image folders at the location {}'.format(img_path))
+    print('######################')
     #  apply function
     image_list, label_list = load(image_paths, verbose = 10000)
     #  Binarize the labels
     lb = LabelBinarizer()
     label_list = lb.fit_transform(label_list)
-    # split train test
-    # X_train,X_test, y_train, y_test = train_test_split(image_list,
-    #                                                 label_list,
-    #                                                 test_size=0.1,
-    #                                                 random_state=37)
     X_train, y_train =  image_list, label_list
     batched_data = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(len(y_train))
 
@@ -123,26 +125,90 @@ def client_main(dir_path, scaling_factor=10):
                         optimizer=optimizer,
                         metrics=metrics)
     # Load weights from h5 file
-    local_model.load_weights('/home/nextg3/Documents/Thesis/Code/FL/weights.h5')
+    print('loading global weight at: {}'.format(os.path.join(folder_path,'global_model.h5')))
+    local_model.load_weights(os.path.join(folder_path,'global_model.h5'))
     # Fit local model with client data
     local_model.fit(batched_data, epochs=1, verbose=0)
-    # scale the model weights and add to the list
-    # scaling_factor = weight_scaling_factor(clients_batched, client)
-    scaled_weights = scale_model_weights(local_model.get_weights(),scaling_factor)
-    return scaled_weights
+    client_name = client_name+'_weights.h5'
+    print('Saving trained model weights as: {}'.format(os.path.join(folder_path,client_name)))
+    local_model.save_weights(os.path.join(folder_path,client_name))
+    print('######################')
+    print('######################')
 
 
+def miner_main(folder_path='/home/nextg3/Documents/Thesis/Code/FL/local_models'):
+    # Declare Model parameters
+    print('######################')
+    print('######################')
+    print('I am a miner')
+    print('######################')
+    print('######################')
+    lr = 0.01
+    loss = 'categorical_crossentropy'
+    metrics = ['accuracy']
+    optimizer = SGD(learning_rate = lr,
+                    momentum = 0.9
+                    )
+    # Read all the h5 files from the path
+    weight_path = folder_path
+    # Get path list
+    weight_paths = list(paths.list_files(weight_path))
+    local_models  = []
 
-    # scaled_local_weightsmlp_local = SimpleMLP()
-    # local_model = smlp_local.build(784,10)
-    # local_model.compile(loss=loss,
-    #                     optimizer=optimizer,
-    #                     metrics=metrics)
+    # get all models
+    for p in weight_paths:
+        if 'global_model' in p:
+            continue
+        test_model = SimpleMLP()
+        local_model = test_model.build(784,10)
+        local_model.compile(loss=loss,
+                            optimizer=optimizer,
+                            metrics=metrics)
+        # set weights of the global model as weights of the local modelk
 
-def miner_main(global_model, scaled_local_weight_list):
-    average_weights = sum_scaled_weights(scaled_local_weight_list)
-    # update global model
+        local_model.load_weights(p)
+        scaled_model_weights = scale_model_weights(local_model.get_weights(),len(weight_paths))
+        local_models.append(scaled_model_weights)
+        k.clear_session()
+
+    # Final weights
+    average_weights = sum_scaled_weights(local_models)
+    smlp_global = SimpleMLP()
+    global_model = smlp_global.build(784,10)
     global_model.set_weights(average_weights)
+    print('######################')
+    print('######################')
+    print('Saving global weights at {}'.format(os.path.join(folder_path,'global_model.h5')))
+    global_model.save_weights(os.path.join(folder_path,'global_model.h5'))
+    print('######################')
+    print('######################')
+
+
+
+if __name__=='__main__':
+    try:
+      opts, args = getopt.getopt(sys.argv[1:],"he:p:i:",["entity=","path=","image_path="])
+    except getopt.GetoptError:
+        print ('script.py -e <miner/client> -p <path for shared storage> -ip <path to folder with local images>')
+        sys.exit(2)
+    miner = False
+    for opt, arg in opts:
+      if opt == '-h':
+         print ('script.py -e <miner/client> -p <path for shared storage> -ip <path to folder with local images>')
+         sys.exit()
+      elif opt in ("-e", "--entity"):
+         if arg == 'miner':
+            miner=True
+      elif opt in ("-p", "--path"):
+         folder_path = arg
+      elif opt in ("-i", "--image_path"):
+         imag_path = arg
+    
+    if miner:
+        miner_main(folder_path)
+    else:
+        client_main(folder_path=folder_path,img_path=imag_path,client_name='bob')
+
 
 
 
